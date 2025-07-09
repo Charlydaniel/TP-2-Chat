ff33b586f577ec4e5abe4f82809eaa0542b2d595
Charly Peralta <dan.bsas22@gmail.com>
2025-07-09 11:08:07 -0300

    Subiendo enrutamiento

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import { BrowserRouter } from 'react-router'
 createRoot(document.getElementById('root')).render(
 
 
-<BrowserRouter>
+<BrowserRouter basename="/TP-2-Chat">
         <ContactContextProvider>
                 <MessageContextProvider>
                         <ContactDataContextProvider>
